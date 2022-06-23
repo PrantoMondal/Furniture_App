38430c0dc9dcedee85dd840157bec36db1feed74
PrantoMondal <prantomondal517@gmail.com>
2022-06-23 14:31:23 +0600

    model class added

---
+++ lib/models/models.dart
@@ -0,0 +1,20 @@
+class Furniture{
+  int? id;
+  String? name;
+  String? subTitle;
+  String? details;
+  String? type;
+  double? price;
+  String? image;
+  double? rating;
+
+  Furniture(
+      {this.id,
+      this.name,
+      this.subTitle,
+      this.details,
+      this.type,
+      this.price,
+      this.image,
+      this.rating});
+}
