4a36ad91e28f70b1ba826860e8f0a13e8088e23c
PrantoMondal <prantomondal517@gmail.com>
2022-06-24 01:16:17 +0600

    List view added

---
+++ lib/details_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class FurnitureDetails extends StatefulWidget {
+  const FurnitureDetails({Key? key}) : super(key: key);
+
+  @override
+  State<FurnitureDetails> createState() => _FurnitureDetailsState();
+}
+
+class _FurnitureDetailsState extends State<FurnitureDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_app/db/temp_db.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,9 +15,40 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(title: Text('Furniture Dev'),),
       body: ListView(
-        children: [
 
-        ],
+        children: furnitureList.map((furniture) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            tileColor: Colors.blue.shade100,
+            onTap: (){
+
+            },
+            title: Text(furniture.name!),
+            subtitle: Text(furniture.details!),
+            leading: Image.asset(furniture.image!,fit: BoxFit.fill),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Column(
+                  children: [
+                    Text(furniture.rating!.toString()),
+                    Row(
+                      children: [
+                        Text('\$',style: TextStyle(height: 1.5, fontSize: 20)),
+                        Text(furniture.price!.toString(),style: TextStyle(height: 1.5, fontSize: 20)),
+                      ],
+                    ),
+                  ],
+                ),
+
+
+                //Icon(Icons.star,color: index.isEven?Colors.green:Colors.red,)
+              ],
+            ),
+
+          ),
+
+        )).toList(),
       ),
 
     );
