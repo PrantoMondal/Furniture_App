c1bd707562e5a193eb60dc7d2c4ba3f78ec354c7
PrantoMondal <prantomondal517@gmail.com>
2022-07-13 00:38:13 +0600

    50% details page done

--- lib/details.dart
+++ lib/details.dart
@@ -14,50 +14,74 @@ class DetailsPage extends StatelessWidget {
         child: DetailsPageAppBar(),
       ),
       body: Column(
-
         children: [
           Container(
+            alignment: Alignment.topCenter,
+            // color: Colors.cyan.shade50,
+            decoration: BoxDecoration(
+                color: Colors.cyan.shade50,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(31),
+                    bottomRight: Radius.circular(31))),
             child: SizedBox(
-              width: MediaQuery.of(context).size.width*0.8,
-              height: MediaQuery.of(context).size.height*0.4,
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.4,
               child: AspectRatio(
                 aspectRatio: 1,
                 child: Image.asset('assets/images/f1.png'),
               ),
             ),
-          ),Row(
-            children: [
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Radio(
-                    value: 1,
-                    groupValue: AspectRatio,
-                    activeColor: Colors.red,
-                    onChanged: (val) {
+          ),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
 
-                    },
-                  ),
-                  Radio(
-                    value: 2,
-                    groupValue: AspectRatio,
-                    activeColor: Colors.blue,
-                    onChanged: (val) {
-
-                    },
-                  ),
-                  Radio(
-                    value: 3,
-                    groupValue: AspectRatio,
-                    activeColor: Colors.blue,
-                    onChanged: (val) {
-
-                    },
-                  ),
-                ],
-              )
-            ],
-          )
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16,left: 25),
+              child: Text(
+                'Mona Chair',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: MediaQuery.of(context).size.height * 0.05,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+      Container(
+        margin: const EdgeInsets.only(
+            top: 5, left: 20.0, right: 10.0),
+        //*****************************************************************        rating
+        child: Row(
+          children: [
+            Icon(
+              Icons.star,
+              color: Colors.amber,
+              size: 20,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.amber,
+              size: 20,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.amber,
+              size: 20,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.amber,
+              size: 20,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.amber,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
         ],
       ),
     );
