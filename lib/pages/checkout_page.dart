7c7c43c4979eaf8ef5375e644bec1c4f2b71ba91
PrantoMondal <prantomondal517@gmail.com>
2022-07-22 01:09:12 +0600

    card added

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -16,9 +16,13 @@ class _CheckoutPageState extends State<CheckoutPage> {
     final int _selected = 1;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cart', style: TextStyle(color: Colors.black,
-            fontFamily: 'Roboto',
-            fontWeight: FontWeight.bold),),
+        title: Text(
+          'Cart',
+          style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -26,23 +30,21 @@ class _CheckoutPageState extends State<CheckoutPage> {
           padding: const EdgeInsets.only(top: 18.0, left: 22.0),
           child: IconBackBtn(
               iconData: Icons.arrow_back_ios_outlined,
-              press: () => Navigator.pop(context)
-          ),
+              press: () => Navigator.pop(context)),
         ),
         actions: [
           Padding(
             padding: const EdgeInsets.only(top: 18.0, right: 12.0),
-
             child: FlatButton(
-              onPressed: () {
-
-              },
-              child: Icon(Icons.delete_outline, color: Colors.blue,),
+              onPressed: () {},
+              child: Icon(
+                Icons.delete_outline,
+                color: Colors.blue,
+              ),
             ),
           )
         ],
       ),
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -50,29 +52,40 @@ class _CheckoutPageState extends State<CheckoutPage> {
           ListTile(
             leading: SizedBox(
               child: Radio<int>(
-              value: 1,
-              groupValue: _selected,
-              onChanged: (value) {
-                setState(() {
-                });
-              },
+                value: 1,
+                groupValue: _selected,
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
             ),
-            ),
-            title:  Container(
-              color: Color(0xFFDEF5FF),
-              padding: const EdgeInsets.symmetric(vertical: 2),
-              child: Image.asset('assets/images/f1.png'),
+            title: Container(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                color: Color(0xFFDEF5FF),
+                child: Image.asset('assets/images/f1.png'),
+              ),
             ),
             trailing: Padding(
-              padding: const EdgeInsets.only(top:0,left: 20,right: 50),
+              padding: const EdgeInsets.only(top: 0, left: 20, right: 50),
               child: Column(
                 children: [
                   Text(
-                    'Mona Chair',style: TextStyle(fontSize: 18,fontFamily: 'Poppins',color: Color(0xFF787878)),
-          ),
+                    'Mona Chair',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'Poppins',
+                        color: Color(0xFF787878)),
+                  ),
                   Text(
-                    '\$ 50.50',style: TextStyle(fontSize: 16,fontFamily: 'Poppins',color: Color(0xFF1A1919)),
-          ),
+                    '\$ 50.50',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        color: Color(0xFF1A1919)),
+                  ),
                 ],
               ),
             ),
@@ -80,7 +93,5 @@ class _CheckoutPageState extends State<CheckoutPage> {
         ],
       ),
     );
-    
   }
 }
-
